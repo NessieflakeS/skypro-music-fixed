8242bc6634408e5fb363e88724192730b1c61a00
NessieflakeS <nikandrof.danil@gmail.com>
2026-02-10 21:08:27 +0800

    .

--- store/store.ts
+++ store/store.ts
@@ -2,6 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
 import playerReducer from './playerSlice';
 
+export const store = configureStore({
+  reducer: {
+    user: userReducer,
+    player: playerReducer,
+  },
+});
+
 export const makeStore = () => {
   return configureStore({
     reducer: {
@@ -12,5 +19,5 @@ export const makeStore = () => {
 };
 
 export type AppStore = ReturnType<typeof makeStore>;
-export type RootState = ReturnType<AppStore['getState']>;
-export type AppDispatch = AppStore['dispatch'];
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
